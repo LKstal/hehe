8f5ba7b0010611fb3f393bcb4a049d39da91dca3
lz <lz@PC007>
2012-11-04 21:07:58 +0800

    ok

---
+++ src/org/wenke/test/MyTest.java
@@ -0,0 +1,13 @@
+package org.wenke.test;
+
+public class MyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my test");
+	}
+
+}
